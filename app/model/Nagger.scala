8ce97071d76cc3e179637b8eb116a2234f46f860
steve <ssaanen@gmail.com>
2015-04-23 00:13:59 +0200

    added cru for naggers

--- app/model/Nagger.scala
+++ app/model/Nagger.scala
@@ -5,6 +5,21 @@ import controllers.{Assets, routes}
 /**
  * Created by steven on 22/04/15.
  */
-case class Nagger(name: String, phone:String) {
-  var avatarSrc:String = routes.Assets.at("images/favicon.png").url
+class Nagger(_name: String, _phone:String) {
+  var avatarSrc: String = routes.Assets.at("images/favicon.png").url
+  var id: Int = IDGen.get
+  var name = _name
+  var phone = _phone
+}
+
+object Nagger {
+  def apply(name: String, phone:String) = new Nagger(name, phone)
+}
+
+object IDGen {
+  var idCount:Int = 0
+  def get = {
+    idCount += 1
+    idCount - 1
+  }
 }

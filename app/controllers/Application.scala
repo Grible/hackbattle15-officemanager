4a93ab1afe1abb1f152bab81a5c2cedc7514912a
steve <ssaanen@gmail.com>
2015-04-23 01:59:48 +0200

    nagger cru is now one-page (using redirects)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,36 +10,6 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def listNaggers = Action {
-    Ok(views.html.naggers(Naggers.all))
-  }
 
-  def addNagger = Action { request =>
-    val name = request.body.asFormUrlEncoded.get.get("name").get.head
-    val phone = request.body.asFormUrlEncoded.get.get("phone").get.head
-    Naggers.add(Nagger(name, phone))
-    Ok("Nagger added")
-  }
-
-  def updateNagger(id:Int) = Action { request =>
-    val name = request.body.asFormUrlEncoded.get.get("name").get.head
-    val phone = request.body.asFormUrlEncoded.get.get("phone").get.head
-    val nagger = Naggers.get(id.toInt)
-    nagger.name = name
-    nagger.phone = phone
-    Ok("Nagger updated")
-  }
-
-  object Naggers {
-    var all: Set[Nagger] = Set()
-
-    add(Nagger("Gino", "0612345678"))
-    add(Nagger("Leon", ""))
-
-    def add(nagger: Nagger) = all = all + nagger
-    def get(id:Int) = all.find(_.id == id).get
-
-    override def toString = all.toString
-  }
 
 }
---
+++ app/controllers/Nagger.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import controllers.Application._
+import play.api.mvc._
+
+/**
+ * Created by steven on 23/04/15.
+ */
+object Nagger extends Controller {
+  def addNagger = Action { request =>
+    val name = request.body.asFormUrlEncoded.get.get("name").get.head
+    val phone = request.body.asFormUrlEncoded.get.get("phone").get.head
+    Naggers.add(model.Nagger(name, phone))
+    Redirect(routes.Nagger.listNaggers())
+  }
+
+  def listNaggers = Action {
+    Ok(views.html.naggers(Naggers.all))
+  }
+
+  def updateNagger(id:Int) = Action { request =>
+    val name = request.body.asFormUrlEncoded.get.get("name").get.head
+    val phone = request.body.asFormUrlEncoded.get.get("phone").get.head
+    val nagger = Naggers.get(id.toInt)
+    nagger.name = name
+    nagger.phone = phone
+    Redirect(routes.Nagger.listNaggers())
+  }
+
+  object Naggers {
+    var all: Set[model.Nagger] = Set()
+
+    add(model.Nagger("Gino", "0612345678"))
+    add(model.Nagger("Leon", ""))
+
+    def add(nagger: model.Nagger) = all = all + nagger
+    def get(id:Int) = all.find(_.id == id).get
+
+    override def toString = all.toString
+  }
+
+}

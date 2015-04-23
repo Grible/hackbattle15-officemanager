9f45508ab1e805e2f13bc2b6cdc2d80da4c8e1d6
steve <ssaanen@gmail.com>
2015-04-23 10:49:16 +0200

    added tasks orm & crud

--- app/controllers/NaggerController.scala
+++ app/controllers/NaggerController.scala
@@ -1,17 +1,20 @@
 package controllers
 
 import controllers.Application._
+import model.Nagger
 import play.api.mvc._
 
 /**
  * Created by steven on 23/04/15.
  */
-object Nagger extends Controller {
+object NaggerController extends Controller {
+  val listNaggersRoute = routes.NaggerController.listNaggers()
+
   def addNagger = Action { request =>
     val name = request.body.asFormUrlEncoded.get.get("name").get.head
     val phone = request.body.asFormUrlEncoded.get.get("phone").get.head
-    Naggers.add(model.Nagger(name, phone))
-    Redirect(routes.Nagger.listNaggers())
+    Naggers.add(Nagger(name, phone))
+    Redirect(listNaggersRoute)
   }
 
   def listNaggers = Action {
@@ -24,21 +27,21 @@ object Nagger extends Controller {
     val nagger = Naggers.get(id.toInt)
     nagger.name = name
     nagger.phone = phone
-    Redirect(routes.Nagger.listNaggers())
+    Redirect(listNaggersRoute)
   }
 
   def deleteNagger(id:Int) = Action {
     Naggers.delete(id)
-    Redirect(routes.Nagger.listNaggers())
+    Redirect(listNaggersRoute)
   }
 
   object Naggers {
-    var all: Set[model.Nagger] = Set()
+    var all: Set[Nagger] = Set()
 
-    add(model.Nagger("Gino", "0612345678"))
-    add(model.Nagger("Leon", ""))
+    add(Nagger("Gino", "0612345678"))
+    add(Nagger("Leon", ""))
 
-    def add(nagger: model.Nagger) = all = all + nagger
+    def add(nagger: Nagger) = all = all + nagger
     def get(id:Int) = all.find(_.id == id).get
     def delete(id:Int) = all = all - get(id)
 

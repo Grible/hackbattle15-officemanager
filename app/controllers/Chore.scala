5496525138260ea891de53d8f751d3c07a97bb5b
Gino Mangnoesing <gvh.sing@gmail.com>
2015-04-23 11:16:48 +0200

    Added create chore view

--- app/controllers/Chore.scala
+++ app/controllers/Chore.scala
@@ -2,10 +2,6 @@ package controllers
 
 import play.api.mvc._
 
-/**
- * Created by Gino on 23-04-15.
- */
-
 object Chore extends Controller {
   def add = Action {
     Ok(views.html.chore("Your new application is ready."))
